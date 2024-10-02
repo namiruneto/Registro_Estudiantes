47b9c1c15db8c3b575eae65f3fa3a214d06279b4
namiruneto <cesfernando1212@gmail.com>
2024-10-01 23:29:33 -0500

    feat(service): servicios del sitio

---
+++ Registro_Estudiantes.Server/Services/AuthService.cs
@@ -0,0 +1,30 @@
+﻿using Registro_Estudiantes.Server.Data;
+using Registro_Estudiantes.Server.Model.Request;
+using Registro_Estudiantes.Server.Repository;
+
+namespace Registro_Estudiantes.Server.Services
+{
+    public class AuthService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool ValidateUser(Login userLogin, out int IdUser)
+        {
+            IdUser = 0;
+            var user = _context.Users.SingleOrDefault(u => u.Username == userLogin.Username && u.Password == userLogin.Password);
+            IdUser = user.Id;
+            return user != null; // Retorna true si se encontró un usuario
+        }
+
+        public bool ValidateUser(AvailableMaterial availableMaterial)
+        {
+            var user = _context.Users.SingleOrDefault(u => u.Id == availableMaterial.Username);
+            return user != null; // Retorna trueSingleOrDefault si se encontró un usuario
+        }
+    }
+}
