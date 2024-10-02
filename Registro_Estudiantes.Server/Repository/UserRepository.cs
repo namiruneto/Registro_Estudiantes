12265c063edc2a81bed4933456e6156c7689fc7e
namiruneto <cesfernando1212@gmail.com>
2024-10-01 23:29:00 -0500

    feat(reposotori): peticiones de usuario

---
+++ Registro_Estudiantes.Server/Repository/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Registro_Estudiantes.Server.Data;
+using Registro_Estudiantes.Server.Model.dto;
+
+namespace Registro_Estudiantes.Server.Repository
+{
+    public class UserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public LoginDto GetUserByUsername(string username)
+        {
+            return _context.Users.SingleOrDefault(u => u.Username == username);
+        }
+    }
+}
