ca4e70ffd89b06edce39a0b5d66af52718d119f4
namiruneto <cesfernando1212@gmail.com>
2024-10-01 23:26:39 -0500

    feat(Controllers): Controlador de Login y Controlador de estudiantes

---
+++ Registro_Estudiantes.Server/Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Registro_Estudiantes.Server.Model.Request;
+using Registro_Estudiantes.Server.Model.dto;
+using Registro_Estudiantes.Server.Services;
+
+namespace Registro_Estudiantes.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        private readonly AuthService _authService;
+
+        public LoginController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] Login userLogin)
+        {
+            if (_authService.ValidateUser(userLogin, out int IdUser))
+            {
+                // Generar y retornar el token aquí
+                return Ok(new { Token = GenerateToken(userLogin.Username, IdUser) }); // Reemplaza con el token real
+            }
+
+            return Unauthorized("Usuario o contraseña incorrectos.");
+        }
+
+        private string GenerateToken(string username, int userId)
+        {
+            // Asegúrate de que la clave tenga al menos 32 bytes
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("R2LzksP8W4qQy39cLQ1Xj8a7b0pD10f5")); // Cambia esta clave a una más segura
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {   new Claim(ClaimTypes.Name, username), // El nombre de usuario
+                 new Claim("UserId", userId.ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: null,
+                audience: null,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(60), // Token expira en 60 minutos
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ Registro_Estudiantes.Server/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Registro_Estudiantes.Server.Model.Request;
+using Registro_Estudiantes.Server.Model.dto;
+using Registro_Estudiantes.Server.Services;
+using Microsoft.EntityFrameworkCore;
+using Registro_Estudiantes.Server.Data;
+
+namespace Registro_Estudiantes.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public StudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("AvailableMaterial")]
+        public IActionResult AvailableMaterial([FromBody] AvailableMaterial userLogin)
+        {
+            //if (_auth.ValidateUser(userLogin))
+            //{
+            var materias = _context.Materias
+   .Select(m => new MateriaDto
+   {
+       MateriaId = m.MateriaId,
+       Nombre = m.Nombre,
+       Creditos = m.Creditos
+   }).ToList();
+
+            return Ok(materias);
+
+
+            //}
+
+            //return null;
+        }
+    }
+}
