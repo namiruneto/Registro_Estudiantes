50d69384a4b14b4a42257f9c47471fbc68b05138
namiruneto <cesfernando1212@gmail.com>
2024-10-01 23:27:57 -0500

    feat(data): configuracion de base de datos

---
+++ Registro_Estudiantes.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro_Estudiantes.Server.Model.dto;
+using System.Collections.Generic;
+
+namespace Registro_Estudiantes.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<LoginDto> Users { get; set; }
+        public DbSet<MateriaDto> Materias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Si MateriaDto es solo un DTO, indícalo como una entidad sin clave
+            modelBuilder.Entity<MateriaDto>().HasNoKey();
+        }
+    }
+}
