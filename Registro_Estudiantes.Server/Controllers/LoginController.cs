5f95cba61a176f9c8a97728da5767e125d6ef1f4
Cesar Fernando Carreño Peña <cesfernando1212@gmail.com>
2024-10-03 09:56:47 -0500

    chose(LoginController) correcion de motodos utilizando principios solid

--- Registro_Estudiantes.Server/Controllers/LoginController.cs
+++ Registro_Estudiantes.Server/Controllers/LoginController.cs
@@ -1,12 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
+﻿using Microsoft.AspNetCore.Mvc;
 using Registro_Estudiantes.Server.Model.Request;
-using Registro_Estudiantes.Server.Model.dto;
 using Registro_Estudiantes.Server.Services;
+using Registro_Estudiantes.Server.Utilitis;
 
 namespace Registro_Estudiantes.Server.Controllers
 {
@@ -23,33 +18,14 @@ namespace Registro_Estudiantes.Server.Controllers
 
         [HttpPost("login")]
         public IActionResult Login([FromBody] Login userLogin)
-        {
+        {            
             if (_authService.ValidateUser(userLogin, out int IdUser))
             {               
-                return Ok(new { Token = GenerateToken(userLogin.Username, IdUser) });
+                return Ok(new { Token = Token.GeneratorToken(userLogin.Username, IdUser) });
             }
 
             return Unauthorized("Usuario o contraseña incorrectos.");
         }
-
-        private string GenerateToken(string username, int userId)
-        {            
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("R2LzksP8W4qQy39cLQ1Xj8a7b0pD10f5"));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-            var claims = new[]
-            {   new Claim(ClaimTypes.Name, username), 
-                 new Claim("UserId", userId.ToString())
-            };
-
-            var token = new JwtSecurityToken(
-                issuer: null,
-                audience: null,
-                claims: claims,
-                expires: DateTime.Now.AddMinutes(60),
-                signingCredentials: creds);
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
+       
     }
 }
